9ac20498bc83c9c210c75d058c97f3ea75c1c74e
Zach Nichols <znichols1131@gmail.com>
2021-11-15 13:05:53 -0500

    created Skill table, skill classes for each data layer, and Skill POST endpoint

---
+++ CharacterCreator.Services/SkillService.cs
@@ -0,0 +1,37 @@
+﻿using CharacterCreator.Data;
+using CharacterCreator.Models.Skill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreator.Services
+{
+    public class SkillService
+    {
+        private readonly Guid _playerId;
+
+        public SkillService(Guid playerId)
+        {
+            _playerId = playerId;
+        }
+
+        public bool CreateSkill(SkillCreate model)
+        {
+            var entity =
+                new Skill()
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    AbilityType = model.AbilityType
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Skills.Add(entity);
+                return ctx.SaveChanges() > 0;
+            }
+        }
+    }
+}
